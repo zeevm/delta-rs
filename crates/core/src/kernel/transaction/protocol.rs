153cab3c076f2b21b94c66ed2129120bc8bd7329
Ze'ev Maor <zeevm@outlook.com>
2025-08-31 12:44:09 +0300

    enable features
    Signed-off-by: Ze'ev Maor <zeevm@outlook.com>

--- crates/core/src/kernel/transaction/protocol.rs
+++ crates/core/src/kernel/transaction/protocol.rs
@@ -222,7 +222,9 @@ impl ProtocolChecker {
 pub static INSTANCE: LazyLock<ProtocolChecker> = LazyLock::new(|| {
     let mut reader_features = HashSet::new();
     reader_features.insert(ReaderFeature::TimestampWithoutTimezone);
-    // reader_features.insert(ReaderFeature::ColumnMapping);
+    reader_features.insert(ReaderFeature::ColumnMapping);
+    reader_features.insert(ReaderFeature::DeletionVectors);
+
 
     let mut writer_features = HashSet::new();
     writer_features.insert(WriterFeature::AppendOnly);
--- crates/test/src/acceptance/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod data;
-pub mod meta;
-
-pub use data::*;
-pub use meta::*;
